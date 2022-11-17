f630eb6313e3d0aa749a135cc1517288b1f5d0ee
Maria Eduarda Ribeiro Viana <viana.ma.ribeiro@gmail.com>
2022-11-17 15:31:02 -0400

    servicos
    servicos

--- PDO/js/scripts.js
+++ PDO/js/scripts.js
@@ -1,10 +1,11 @@
-function addServico(id, nome, valor){
+function addServico(id, nome, valor) {
     //fechar modal
     bootstrap.Modal.getOrCreateInstance(document.getElementById('modalOS')).hide();
     alert(nome);
-    const tcorpo= document.getElementById('itemOS');
+    const tcorpo = document.getElementById('itemOS');
     const linha = document.createElement('tr');
-
-   linha.innerHTML="<td>"+id+"</td>"+nome+"<td></td><td></td>";
-   tcorpo.appendChild(linha);
+    var inputID= '<td><input name="id[]" value = "'+id+'" readonly></td>';
+    var inputQtde= '<td><input name="qtde[]" value = "1" readonly></td>';
+    linha.innerHTML = inputID+ "</td>" + nome + "<td>150</td>"+inputQtde;
+    tcorpo.appendChild(linha);
 }
